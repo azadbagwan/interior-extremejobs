fa66975cd77307309cabafc4bb5e6236341e0cd4
azadbagwan <azadbagwan2@yahoo.com>
2019-09-19 01:58:39 +0530

    commit 18/9/2019 13:35

--- src/pages/About_us.js
+++ src/pages/About_us.js
@@ -18,9 +18,9 @@ About us
 
 Pune's most trusted name
 
-Sathe and Company was established in 1919 and today the 4th generation of the family continues to build on that trust and history.
 
-We began as a trading house for natural stones and plumbing products and grew to become one of the most recognised names in the industry. In 1981, we opened a first-of-its-kind showroom in Pune selling sanitary ware, bath fittings and tiles. Our Pune showroom is conveniently located in Camp and is open Monday to Saturday from 10 a.m. to 7.30 p.m.
+
+We began as a trading house for natural stones and plumbing products and grew to become one of the most recognised names in the industry. In 2001, we opened a first-of-its-kind showroom in Pune selling sanitary ware, bath fittings and tiles. Our Pune showroom is conveniently located in Camp and is open Monday to Saturday from 10 a.m. to 7.30 p.m.
 
 Today, our award winning company is one of the largest retailers of its kind in India and we offer a wide range of Wall & Floor Tiles, Bath products and Modular Bedrooms and Kitchens.
 
